fdd4224a808141b278b3e5c55726101449e79f22
Artur Gorbachev <artur.gorbachev@microsoft.wsei.edu.pl>
2026-01-29 10:56:00 +0100

    Implement Housing repository and update BookingDbContext for housing management

--- BookingService/Data/BookingDbContext.cs
+++ BookingService/Data/BookingDbContext.cs
@@ -1,6 +1,9 @@
+using BookingService.Housing.Models;
+using Microsoft.EntityFrameworkCore;
+
 namespace BookingService.Data;
 
-public class BookingDbContext
+public class BookingDbContext(DbContextOptions<BookingDbContext> options) : DbContext(options)
 {
-    
+    public DbSet<HousingInfo> Housings { get; set; } = null!;
 }
--- BookingService/Data/HousingRepository.cs
+++ BookingService/Data/HousingRepository.cs
@@ -1,6 +1,40 @@
+using BookingService.Housing.Data;
+using BookingService.Housing.Models;
+using Microsoft.EntityFrameworkCore;
+
 namespace BookingService.Data;
 
-public class HousingRepository
+public class HousingRepository(BookingDbContext context) : IHousingRepository
 {
-    
+    public async Task<IEnumerable<HousingInfo>> GetAll()
+    {
+        return await context.Housings.ToListAsync();
+    }
+
+    public async Task<HousingInfo?> GetById(int id)
+    {
+        return await context.Housings.SingleOrDefaultAsync(h => h.Id == id);
+    }
+
+    public async Task Create(HousingInfo housing)
+    {
+        context.Housings.Add(housing);
+        await context.SaveChangesAsync();
+    }
+
+    public async Task Update(HousingInfo housing)
+    {
+        context.Housings.Update(housing);
+        await context.SaveChangesAsync();
+    }
+
+    public async Task Delete(int id)
+    {
+        HousingInfo? housing = await GetById(id);
+        if (housing is not null)
+        {
+            context.Housings.Remove(housing);
+            await context.SaveChangesAsync();
+        }
+    }
 }
